e331026b9c115be67f05bf828372db631ed1ac26
Steven Hall <stevenhall@scottlogic.co.uk>
2014-04-04 17:08:11 +0100

    Some mock data.

--- src/org/emmerich/rrt/db/ApplicationDatabaseHelper.java
+++ src/org/emmerich/rrt/db/ApplicationDatabaseHelper.java
@@ -53,7 +53,52 @@ public class ApplicationDatabaseHelper extends SQLiteOpenHelper {
 	
 	private void insertMocks(SQLiteDatabase db)
 	{
+		insertMockHolds(db);
+		insertMockTasks(db);
+		insertMockRepetitions(db);
+		insertMockWorkouts(db);
 		
+		insertMockExercises(db);
+		insertMockRests(db);
+	}
+
+	private void insertMockRests(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+//		_id INTEGER PRIMARY KEY, entry_index INTEGER, duration INTEGER, workout_id INTEGER
+		db.execSQL("INSERT INTO rest (_id, entry_index, duration, workout_id) VALUES (0, 1, 60, 0)");
+	}
+
+	private void insertMockExercises(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+//		_id INTEGER PRIMARY KEY, entry_index INTEGER, workout_id INTEGER, hold_id INTEGER, task_id INTEGER, repetition_id INTEGER
+		db.execSQL("INSERT INTO exercise (_id, entry_index, workout_id, hold_id, task_id, repetition_id) VALUES (0, 0, 0, 1, 3, 0)");
+	}
+
+	private void insertMockWorkouts(SQLiteDatabase db) {
+		db.execSQL("INSERT INTO workout (_id, name) VALUES (0, 'Test Workout')");
+	}
+
+	private void insertMockRepetitions(SQLiteDatabase db) {
+		db.execSQL("INSERT INTO repetition (_id, type, count) VALUES (0, 'count', 10)");
+		db.execSQL("INSERT INTO repetition (_id, type, count) VALUES (1, 'time', 60)");
+	}
+
+	private void insertMockTasks(SQLiteDatabase db) {
+		db.execSQL("INSERT INTO task (_id, name) VALUES (0, 'Dead Hang')");
+		db.execSQL("INSERT INTO task (_id, name) VALUES (1, 'Bent Arm Hang')");
+		db.execSQL("INSERT INTO task (_id, name) VALUES (2, 'Offset Hang')");
+		db.execSQL("INSERT INTO task (_id, name) VALUES (3, 'Pull-up')");
+		db.execSQL("INSERT INTO task (_id, name) VALUES (4, 'Offset pull-up')");
+		db.execSQL("INSERT INTO task (_id, name) VALUES (5, 'One-arm pull-up')");
+		db.execSQL("INSERT INTO task (_id, name) VALUES (6, 'L-Hang')");
+		db.execSQL("INSERT INTO task (_id, name) VALUES (7, 'Front Lever')");
+	}
+
+	private void insertMockHolds(SQLiteDatabase db) {
+		db.execSQL("INSERT INTO hold (_id, name) VALUES (0, 'Jug')");
+		db.execSQL("INSERT INTO hold (_id, name) VALUES (1, '4-finger Edge')");
+		db.execSQL("INSERT INTO hold (_id, name) VALUES (2, '3-finger Pocket')");
+		db.execSQL("INSERT INTO hold (_id, name) VALUES (3, '2-finger Pocket')");
 	}
 	
 
